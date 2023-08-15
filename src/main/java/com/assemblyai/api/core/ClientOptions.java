87c8a6a1fff85b67ec2f70663138e8fed7e5f639
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2023-08-15 03:08:41 +0000

    Release 0.0.15

--- src/main/java/com/assemblyai/api/core/ClientOptions.java
+++ src/main/java/com/assemblyai/api/core/ClientOptions.java
@@ -26,7 +26,7 @@ public final class ClientOptions {
                 "X-Fern-SDK-Name",
                 "com.assemblyai.fern:api-sdk",
                 "X-Fern-SDK-Version",
-                "0.0.14",
+                "0.0.15",
                 "X-Fern-Language",
                 "JAVA"));
         this.headerSuppliers = headerSuppliers;
