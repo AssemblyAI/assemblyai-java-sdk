d2b7726a9bfbcf55749913b2b4a2be80ae1dcc42
Deep Singhvi <deep@usebirch.com>
2023-08-14 23:06:10 -0400

    add transcriber class (#1)

---
+++ src/main/java/com/assemblyai/api/Transcriber.java
@@ -0,0 +1,83 @@
+package com.assemblyai.api;
+
+import com.assemblyai.api.core.Environment;
+import com.assemblyai.api.resources.transcript.requests.TranscriptRequest;
+import com.assemblyai.api.types.TranscriptResponse;
+import com.assemblyai.api.types.TranscriptStatus;
+import com.assemblyai.api.types.UploadResponse;
+import java.io.File;
+
+public final class Transcriber {
+
+    private final AssemblyaiApiClient client;
+
+    private Transcriber(AssemblyaiApiClient client) {
+        this.client = client;
+    }
+
+    /**
+     * Transcribes an audio file whose location can be specified via a URL.
+     */
+    public TranscriptResponse transcribe(String url, TranscriptRequest transcriptRequest, boolean poll) {
+        TranscriptResponse transcriptResponse = this.client
+                .transcript()
+                .create(TranscriptRequest.builder()
+                        .from(transcriptRequest)
+                        .audioUrl(url)
+                        .build());
+        if (poll) {
+            return awaitCompletion(transcriptResponse.getId());
+        }
+        return transcriptResponse;
+    }
+
+    /**
+     * Transcribes an audio file whose location can be specified via a filepath.
+     */
+    public TranscriptResponse transcribe(File data, TranscriptRequest transcriptRequest, boolean poll) {
+        UploadResponse uploadResponse = this.client.files().upload(data);
+        return transcribe(uploadResponse.getUploadUrl(), transcriptRequest, poll);
+    }
+
+    private TranscriptResponse awaitCompletion(String transcriptId) {
+        try {
+            while (true) {
+                TranscriptResponse transcript = this.client.transcript().get(transcriptId);
+                if (transcript.getStatus().isPresent()
+                        && transcript.getStatus().get().equals(TranscriptStatus.COMPLETED)) {
+                    return transcript;
+                }
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Interrupted while polling transcript id=" + transcriptId);
+        }
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static final class Builder {
+        private final AssemblyaiApiClientBuilder clientBuilder = new AssemblyaiApiClientBuilder();
+
+        public Builder apiKey(String apiKey) {
+            this.clientBuilder.apiKey(apiKey);
+            return this;
+        }
+
+        public Builder environment(Environment environment) {
+            this.clientBuilder.environment(environment);
+            return this;
+        }
+
+        public Builder url(String url) {
+            this.clientBuilder.url(url);
+            return this;
+        }
+
+        public Transcriber build() {
+            return new Transcriber(clientBuilder.build());
+        }
+    }
+}
--- src/main/java/com/assemblyai/api/resources/files/FilesClient.java
+++ src/main/java/com/assemblyai/api/resources/files/FilesClient.java
@@ -5,10 +5,14 @@ import com.assemblyai.api.core.ClientOptions;
 import com.assemblyai.api.core.ObjectMappers;
 import com.assemblyai.api.core.RequestOptions;
 import com.assemblyai.api.types.UploadResponse;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import okhttp3.Headers;
 import okhttp3.HttpUrl;
+import okhttp3.MediaType;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class FilesClient {
@@ -18,22 +22,23 @@ public class FilesClient {
         this.clientOptions = clientOptions;
     }
 
-    public UploadResponse upload() {
-        return upload(null);
+    public UploadResponse upload(File file) {
+        return upload(file, null);
     }
 
-    public UploadResponse upload(RequestOptions requestOptions) {
+    public UploadResponse upload(File file, RequestOptions requestOptions) {
         HttpUrl _httpUrl = HttpUrl.parse(this.clientOptions.environment().getUrl())
                 .newBuilder()
                 .addPathSegments("upload")
                 .build();
-        Request _request = new Request.Builder()
-                .url(_httpUrl)
-                .method("POST", null)
-                .headers(Headers.of(clientOptions.headers(requestOptions)))
-                .addHeader("Content-Type", "application/json")
-                .build();
         try {
+            byte[] _body = Files.readAllBytes(file.toPath());
+            Request _request = new Request.Builder()
+                    .url(_httpUrl)
+                    .method("POST", RequestBody.create(MediaType.get("application/octet-stream"), _body))
+                    .headers(Headers.of(clientOptions.headers(requestOptions)))
+                    .addHeader("Content-Type", "application/json")
+                    .build();
             Response _response = clientOptions.httpClient().newCall(_request).execute();
             if (_response.isSuccessful()) {
                 return ObjectMappers.JSON_MAPPER.readValue(_response.body().string(), UploadResponse.class);
