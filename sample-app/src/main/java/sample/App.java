ead210f2a7bb07a28077c05d04b8fc0cdf9ee021
dsinghvi <dsinghvi@umich.edu>
2023-12-01 16:51:08 -0500

    bring back app

--- sample-app/src/main/java/sample/App.java
+++ sample-app/src/main/java/sample/App.java
@@ -20,9 +20,55 @@ import java.nio.file.Files;
 public final class App {
 
     public static void main(String... args) throws IOException {
+        AssemblyAI aai = AssemblyAI.builder()
+                .apiKey(System.getenv("ASSEMBLY_AI_API_KEY"))
+                .build();
+
+        Transcriber transcriber = Transcriber.builder()
+                .apiKey(System.getenv("ASSEMBLY_AI_API_KEY"))
+                .build();
+
+        Transcript transcript =
+                transcriber.transcribe("https://storage.googleapis.com/aai-docs-samples/nbc.mp3");
+        System.out.println(transcript);
+
+        SentencesResponse sentences = aai.transcript().getSentences(transcript.getId());
+        System.out.println("Get transcript sentences. " + sentences);
+
+        ParagraphsResponse paragraphs = aai.transcript().getParagraphs(transcript.getId());
+        System.out.println("Get transcript paragraphs. " + paragraphs);
+
+        String srt = aai.transcript().getSubtitles(transcript.getId(), SubtitleFormat.SRT);
+        System.out.println("Get transcript srt. " + srt);
+
+        String vtt = aai.transcript().getSubtitles(transcript.getId(), SubtitleFormat.VTT);
+        System.out.println("Get transcript vtt. " + vtt);
+
+        WordSearchResponse search = aai.transcript().wordSearch(transcript.getId(), TranscriptWordSearchRequest.builder()
+                .words("NBC")
+                .build());
+        System.out.println("Search transcript. " + search);
+
+        transcript = aai.transcript().delete(transcript.getId());
+        System.out.println("Delete transcript. " + transcript);
+
+        File file = new File("sample-app/src/main/resources/nZP7pb_t4oA.mp3");
+        UploadedFile uploadedFile = aai.files().upload(Files.readAllBytes(file.toPath()));
+        System.out.println("Uploaded file" + uploadedFile);
+
+        transcript = aai.transcript().create(CreateTranscriptParameters.builder()
+                .audioUrl("https://storage.googleapis.com/aai-docs-samples/nbc.mp3")
+                .build());
+        System.out.println("Created transcript " + transcript);
+
+        transcript = aai.transcript().get(transcript.getId());
+        System.out.println("Got transcript. " + transcript);
+
+        TranscriptList transcripts = aai.transcript().list();
+        System.out.println("List transcript. " + transcripts);
+
         RealtimeTranscriber realtimeTranscriber = RealtimeTranscriber.builder()
-                .apiKey("cf33ed71255b4cfdb7dfd98ef5b5dd41")
-                .onSessionStart(System.out::println)
+                .apiKey(System.getenv("ASSEMBLY_AI_API_KEY"))
                 .onPartialTranscript(System.out::println)
                 .onFinalTranscript(System.out::println)
                 .onError((err) -> {
