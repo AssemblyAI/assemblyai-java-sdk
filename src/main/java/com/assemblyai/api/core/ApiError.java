a4f715ccfaeda2595c250e8be8400353dfb58137
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2024-07-25 13:11:40 +0900

    Add ApiError back for backwards compat

---
+++ src/main/java/com/assemblyai/api/core/ApiError.java
@@ -0,0 +1,42 @@
+package com.assemblyai.api.core;
+
+/**
+ * This exception type will be thrown for any non-2XX API responses.
+ */
+public class ApiError extends AssemblyAIException {
+    /**
+     * The error code of the response that triggered the exception.
+     */
+    private final int statusCode;
+
+    /**
+     * The body of the response that triggered the exception.
+     */
+    private final Object body;
+
+    public ApiError(String message, int statusCode, Object body) {
+        super(message);
+        this.statusCode = statusCode;
+        this.body = body;
+    }
+
+    /**
+     * @return the statusCode
+     */
+    public int statusCode() {
+        return this.statusCode;
+    }
+
+    /**
+     * @return the body
+     */
+    public Object body() {
+        return this.body;
+    }
+
+    @java.lang.Override
+    public String toString() {
+        return "AssemblyAIApiException{" + "message: " + getMessage() + ", statusCode: " + statusCode + ", body: "
+                + body + "}";
+    }
+}
